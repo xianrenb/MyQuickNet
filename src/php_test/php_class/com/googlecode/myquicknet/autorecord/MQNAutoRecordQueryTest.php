b108d09c8965f77d5dcc38f11f02693d09f5b1fc
peter <xianrenb@gmail.com>
2012-07-25 10:54:56 +0800

    removed empty docblocks

--- src/php_test/php_class/com/googlecode/myquicknet/autorecord/MQNAutoRecordQueryTest.php
+++ src/php_test/php_class/com/googlecode/myquicknet/autorecord/MQNAutoRecordQueryTest.php
@@ -7,9 +7,6 @@
  * @copyright (c) 2012 MyQuickNet Development Group
  * @license http://www.opensource.org/licenses/gpl-3.0.html GNU General Public License version 3 (GPLv3)
  */
-/**
- *
- */
 
 /**
  * Test class for MQNAutoRecordQuery.
