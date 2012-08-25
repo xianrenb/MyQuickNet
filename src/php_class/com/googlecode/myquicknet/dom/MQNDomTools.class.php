e65dceb5147cad58b633df31c436b09044b9a12c
peter <xianrenb@gmail.com>
2012-08-25 14:42:57 +0800

    added MQNDomTools

---
+++ src/php_class/com/googlecode/myquicknet/dom/MQNDomTools.class.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * MQNDomTools
+ * @package MyQuickNet
+ * @version 3.0
+ * @copyright (c) 2012 MyQuickNet Development Group
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace com\googlecode\myquicknet\dom;
+
+/**
+ *
+ */
+class MQNDomTools {
+
+    /**
+     *
+     * @var array|null
+     */
+    private $numericEntityArray;
+
+    /**
+     *
+     * @param array $config 
+     */
+    public function __construct(array $config = array()) {
+        $this->resetNumericEntityArray();
+    }
+
+    /**
+     *
+     * @param string $html
+     * @return string 
+     */
+    public function convertNamedEntityToNumericEntity($html) {
+        new \String($html);
+        $numericEntityArray = $this->numericEntityArray;
+
+        $callback = function (array $matches) use ($numericEntityArray) {
+                    $namedEntity = (string) $matches[0];
+
+                    if (array_key_exists($namedEntity, $numericEntityArray)) {
+                        $numericEntity = (string) $numericEntityArray[$namedEntity];
+                    } else {
+                        $numericEntity = '&#160;';
+                    }
+
+                    return $numericEntity;
+                };
+
+        $html = (string) preg_replace_callback('/&[A-Za-z][0-9A-Za-z]+;/', $callback, $html);
+        return $html;
+    }
+
+    /**
+     * 
+     */
+    public function resetNumericEntityArray() {
+        $this->numericEntityArray = array();
+        $translationTable = get_html_translation_table(HTML_ENTITIES);
+
+        foreach ($translationTable as $key => $value) {
+            $numericEntity = (string) mb_encode_numericentity($key, array(0x0, 0xffff, 0, 0xffff), 'HTML-ENTITIES');
+            $this->numericEntityArray[$value] = $numericEntity;
+        }
+    }
+
+    /**
+     *
+     * @param string $key
+     * @param string $value 
+     */
+    public function setNumericEntityArray($key, $value) {
+        new \String($key);
+        new \String($value);
+        $this->numericEntityArray[$key] = (string) $value;
+    }
+
+}
+
+?>
---
+++ src/php_test/php_class/com/googlecode/myquicknet/dom/MQNDomToolsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * MQNDomToolsTest
+ * @package MyQuickNet
+ * @version 3.0
+ * @copyright (c) 2012 MyQuickNet Development Group
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace com\googlecode\myquicknet\dom;
+
+/**
+ * Test class for MQNDomTools.
+ */
+class MQNDomToolsTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp() {
+        
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown() {
+        
+    }
+
+    public function test1() {
+        $domTools = new MQNDomTools();
+        $this->assertTrue($domTools instanceof MQNDomTools);
+        $html = 'abc abc&quot;abc&nbsp;abc&yuml;abc&badnamedentity;abc';
+        $actual = (string) $domTools->convertNamedEntityToNumericEntity($html);
+        $expected = 'abc abc&#34;abc&#160;abc&#255;abc&#160;abc';
+        $this->assertEquals($expected, $actual);
+        $domTools->setNumericEntityArray('&customnamedentity;', '&#8888;');
+        $html = 'abc abc&quot;abc&nbsp;abc&yuml;abc&customnamedentity;abc';
+        $actual = (string) $domTools->convertNamedEntityToNumericEntity($html);
+        $expected = 'abc abc&#34;abc&#160;abc&#255;abc&#8888;abc';
+        $this->assertEquals($expected, $actual);
+        $domTools->resetNumericEntityArray();
+        $actual = (string) $domTools->convertNamedEntityToNumericEntity($html);
+        $expected = 'abc abc&#34;abc&#160;abc&#255;abc&#160;abc';
+        $this->assertEquals($expected, $actual);
+    }
+
+}
+
+?>
