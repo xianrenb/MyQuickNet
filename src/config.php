5fef5576d1d17482bfa300c2bde3511bc9c0c4b8
peter <xianrenb@gmail.com>
2014-08-24 16:55:59 +0800

    changed to use PSR-0 autoloader example implementation

--- src/config.php
+++ src/config.php
@@ -32,8 +32,18 @@ call_user_func(
 
             spl_autoload_register(
                     function ($className) {
-                        $className = (string) str_replace('\\', DIRECTORY_SEPARATOR, $className);
-                        include_once($className . '.php');
+                        $className = ltrim($className, '\\');
+                        $fileName = '';
+                        $namespace = '';
+
+                        if ($lastNsPos = strrpos($className, '\\')) {
+                            $namespace = substr($className, 0, $lastNsPos);
+                            $className = substr($className, $lastNsPos + 1);
+                            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+                        }
+
+                        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+                        require $fileName;
                     }
             );
         }
